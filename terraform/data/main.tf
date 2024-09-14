ae5184121edf7a1d6c0c71d50018c5e841f1ac6e
Akas Patranobis <akaspatranobis@yahoo.com>
2024-09-14 12:12:45 +0530

    up

--- terraform/data/main.tf
+++ terraform/data/main.tf
@@ -1,24 +1,32 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+// data "aws_ami" "ubuntu" {
+//   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+//   filter {
+//     name   = "name"
+//     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+//   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+//   filter {
+//     name   = "virtualization-type"
+//     values = ["hvm"]
+//   }
 
-  owners = ["099720109477"] # Canonical
+//   owners = ["099720109477"] # Canonical
+// }
+
+// resource "aws_instance" "web" {
+//   ami           = data.aws_ami.ubuntu.id
+//   instance_type = "t2.micro"
+
+//   tags = {
+//     Name = "HelloWorld"
+//   }
+// }
+
+data "aws_security_group" "selected" {
+    name = "allow-all"
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+output "sg" {
+    value = data.aws_security_group.selected
 }
